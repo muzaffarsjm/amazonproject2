b1471b008521251d2b604d88722c79051e473deb
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-11 08:27:29 +0900

    cart multiple selection added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) =>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class = "js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -58,7 +58,9 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
   button.addEventListener('click', ()=>{
-   const productId =  button.dataset.productId;
+    const productId =  button.dataset.productId;
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    let productsAdded = quantitySelector.value;
 
   let matchingItem;
    cart.forEach((item) =>{
@@ -72,7 +74,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
    }else{
     cart.push({
       productId: productId,
-      quantity: 1 
+      //quantity: 1
+      quantity: parseInt(productsAdded) 
      }); 
     };
 
@@ -81,7 +84,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
       cartQuantity += item.quantity;
     });
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-     console.log(cart)
   });
 }); 
 
