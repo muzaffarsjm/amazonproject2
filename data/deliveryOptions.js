07c2b14c7aa82ff7cfcb26d74b859d78c27243ef
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-13 14:30:17 +0900

    delievery options array created

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+const deliveryOptions = [{
+  id: '1',
+  deliverDays: 7,
+  proceCents: 0
+},{
+  id: '2',
+  deliverDays: 3,
+  proceCents: 499
+}, {
+  id: '3',
+  deliverDays: 1,
+  proceCents: 999
+}];
